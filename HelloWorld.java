4520c6c039964bf0ded586de31207eaf12e3a8dc
Deepak-2995 <148603922+Deepak-2995@users.noreply.github.com>
2023-10-21 16:42:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	    System.out.println("Hello World This is first project");
+	    System.out.println("Hello World ");
 
 	}
 
